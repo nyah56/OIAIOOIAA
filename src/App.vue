c847faea9814cecf8aaab039f2ebae5a8be46683
= <x6perb@gmail.com>
2024-12-16 16:16:22 +0700

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import OIAOOIAOO from './components/OIAOOIAOO.vue';
+</script>
+
+<template>
+
+  <!-- ... -->
+  <div class="h-screen flex justify-center items-center">
+    <OIAOOIAOO />
+  </div>
+
+</template>
+
+<style scoped></style>
---
+++ src/components/OIAOOIAOO.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+     
+    <img src="../assets/cat.png" class="w-48 h-48 rounded-full object-cove" alt="OIAOO" srcset="">
+    <video src="../assets/cat.webm" class="w-48 h-48 rounded-full object-cover" autoplay loop muted></video>
+    <!-- <div class="bg-linear-45 from-indigo-500 via-purple-500 to-pink-500"></div> -->
+
+</template>
+<script>
+
+</script>
