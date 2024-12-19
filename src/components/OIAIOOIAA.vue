231c3ac633229285314a109bc7c768c8ba582ceb
= <x6perb@gmail.com>
2024-12-19 09:00:45 +0700

    Make it like a vue code & adding date in Bahasa

--- src/components/OIAIOOIAA.vue
+++ src/components/OIAIOOIAA.vue
@@ -2,18 +2,23 @@
 <template>
 
 
-    <div
-        class="flex-col flex justify-center items-center">
-        <img src="/cat.png" class="w-48 h-48 rounded-full object-cover animate-car" alt="OIAOO" srcset=""
-            v-if="toggleActive">
-        <video src="/cat.webm" class="w-48 h-48 rounded-full object-cover" autoplay loop muted v-else preload></video>
-    <div>
-        <button class="bg-blue-500 text-white py-2 px-4 rounded-md cursor-pointer mt-8 hover:bg-cyan-500"
-            @pointerdown="toggleActive = false" @pointerup="toggleActive = true" @pointerleave="toggleActive = true">
-           <IconPlay v-if="toggleActive"/>
-           <IconPause v-else/>
-        </button>
-    
+    <div class="flex-col flex justify-center items-center">
+        <h3>{{ dayName(date,'id') }}</h3>
+        <h4>{{ dateNow }}</h4>
+            <img src="/cat.png" class="w-48 h-48 rounded-full object-cover animate-car" alt="OIAOO" srcset=""
+                v-if="toggleActive">
+            <div v-else>
+                <video src="/cat.webm" class="w-48 h-48 rounded-full object-cover" autoplay loop muted preload></video>
+                <audio :src="srcSound" ref="carSound" @ended="loopCarSound"></audio>
+            </div>
+        <div>
+            <button class="bg-blue-500 text-white py-2 px-4 rounded-md cursor-pointer mt-8 hover:bg-cyan-500"
+                @pointerdown="toggleActive = false" 
+                @pointerup="toggleActive = true" 
+                @pointerleave="toggleActive = true">
+            <IconPlay v-if="toggleActive"/>
+            <IconPause v-else/>
+            </button>
        </div>
     </div>
     
@@ -23,27 +28,35 @@ import { ref ,onUpdated } from 'vue';
 import IconPlay from "./icons/IconPlay.vue";
 import IconPause from "./icons/IconPause.vue";
 
+let date = new Date();
+const dateNow = date.toLocaleDateString("id");
+const dayName = (date, locale) =>date.toLocaleDateString(locale, { weekday: 'long' });
+const carSound = ref(null);
+const srcSound = ref('oiiai.mp3');
+// const hours = 6
+// const isDayTime = hours > 5 && hours < 17;
+// if(isDayTime === true){
+//   document.write('day');
+// } else {
+//     document.write('night');
+
+// }
+
 let toggleActive = ref(true);
 const START_TIME_SOUND = 0.19;
-let carSound = new Audio('oiiai.mp3');
+// let carSound = new Audio('oiiai.mp3');
 const loopCarSound = () =>{
-    carSound.preload = true;
-    carSound.currentTime = START_TIME_SOUND;
-    carSound.play();
-    carSound.loop = true;
-    // console.log(carSound.length)
+    carSound.value.currentTime = START_TIME_SOUND;
+    carSound.value.play();
 }
-const resetCarSound = ()=>{
-    carSound.pause();
-    carSound.currentTime = START_TIME_SOUND;
-}
+
 
 onUpdated(() => {
   
   carSpinSound();
 })
 const carSpinSound = () =>{
-    !toggleActive.value?loopCarSound():resetCarSound();
+    !toggleActive.value?loopCarSound():'';
 }
 
 
